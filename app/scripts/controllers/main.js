8946ca8eafa00ba5c127dc37c87513fcf49109fa
unknown <michaelzh@IT-michaelzhu1.istc.ibm.com>
2015-04-16 09:51:19 +0800

    first commit

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,19 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name bloggerApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the bloggerApp
+ */
+angular.module('bloggerApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+
+    $scope.appname = "MIKEBlogger"
+  });
